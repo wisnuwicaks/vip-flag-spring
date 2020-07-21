cb3406aa3223fdfd2580aae393885b1a5ab931ce
wisnuwicaks <32096881+wisnuwicaks@users.noreply.github.com>
2020-07-22 03:50:56 +0700

    create data controller

--- src/main/java/com/cimb/vipflag/controller/CreatedCifController.java
+++ src/main/java/com/cimb/vipflag/controller/CreatedCifController.java
@@ -1,9 +1,32 @@
 package com.cimb.vipflag.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cimb.vipflag.dao.CreatedRepo;
+import com.cimb.vipflag.entity.CreatedData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/created")
+@CrossOrigin
 public class CreatedCifController {
+    @Autowired
+    private CreatedRepo createdRepo;
+
+    @PostMapping("/create_data")
+    public List<CreatedData> createdData (@RequestBody List<CreatedData> listData){
+        Date date = new Date();
+        listData.forEach((data) -> {
+            data.setCreatedDate(date);
+            createdRepo.save(data);
+        });
+        return createdRepo.findAll();
+    }
+
+    @GetMapping("/all_data")
+    public List<CreatedData> allData (){
+        return createdRepo.findAll();
+    }
 }
