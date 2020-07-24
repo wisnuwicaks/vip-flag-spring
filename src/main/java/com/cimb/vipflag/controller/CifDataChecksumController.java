7c577c10184cbb7ac73753ddd031cfdc5864c4d3
wisnuwicaks <32096881+wisnuwicaks@users.noreply.github.com>
2020-07-25 01:26:51 +0700

    generate txt ok

--- src/main/java/com/cimb/vipflag/controller/CifDataChecksumController.java
+++ src/main/java/com/cimb/vipflag/controller/CifDataChecksumController.java
@@ -5,6 +5,8 @@ import com.cimb.vipflag.entity.CifDataChecksum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Date;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,6 +16,7 @@ import java.util.List;
 @RequestMapping("/cifchecksum")
 @CrossOrigin
 public class CifDataChecksumController {
+    private String filePath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\cif_data\\";
 
     @Autowired
     public CifDataChecksumRepo cifDataChecksumRepo;
@@ -21,13 +24,25 @@ public class CifDataChecksumController {
     @PostMapping("/post_data")
     public Iterable<CifDataChecksum> postToTemporaryTable (@RequestBody List<CifDataChecksum> listData){
 
-        LocalDateTime localDateTime = LocalDateTime.now();
-//        LocalDateTime date = LocalDateTime.now();
-//        cifDataChecksumRepo.saveAll(listData);
-        listData.forEach((data) -> {
-            data.setCreatedDate(LocalDateTime.now());
-            cifDataChecksumRepo.save(data);
-        });
+//        LocalDateTime localDateTime = LocalDateTime.now();
+////        LocalDateTime date = LocalDateTime.now();
+////        cifDataChecksumRepo.saveAll(listData);
+//        listData.forEach((data) -> {
+//            data.setCreatedDate(LocalDateTime.now());
+//            cifDataChecksumRepo.save(data);
+//        });
+        try {
+            File myObj = new File(filePath+"filename.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
         return listData;
+
     }
 }
--- src/main/java/com/cimb/vipflag/controller/CifDataController.java
+++ src/main/java/com/cimb/vipflag/controller/CifDataController.java
@@ -1,15 +1,122 @@
 package com.cimb.vipflag.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cimb.vipflag.entity.FileLinkDirectory;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @RestController
-@RequestMapping("/cif")
+@RequestMapping("/cifapprove")
 @CrossOrigin
 public class CifDataController {
 
 
+    private String filePath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\converted_file\\";
+
+    @PostMapping("/file")
+    public void generateTextFile (@RequestBody FileLinkDirectory approvedData) throws IOException {
+//        FileInputStream fis = new FileInputStream(approvedData.getLinkDirectory());
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+//        LocalDate localDate = localDateTime.toLocalDate();
+//        LocalTime localTime = localDateTime.toLocalTime();
+
+
+
+        String localDate = localDateTime.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        String localTime = localDateTime.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+
+        StringBuilder sb = new StringBuilder();
+
+        InputStream fis = new URL(approvedData.getLinkDirectory()).openStream();
+        // Finds the workbook instance for XLSX file
+        XSSFWorkbook myWorkBook = new XSSFWorkbook (fis);
+
+        // Return first sheet from the XLSX workbook
+        XSSFSheet mySheet = myWorkBook.getSheetAt(0);
+
+        // Get iterator to all the rows in current sheet
+        Iterator<Row> rowIterator = mySheet.iterator();
+        sb.append("00"+"|"+ localDate+"|"+localDate+"|"+localTime+"|"+"INCIFVIP");
+        // Traversing over each row of XLSX file
+
+        int lastCol = 0;
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+
+//            System.out.println("ini last col");
+            lastCol = row.getLastCellNum();
+//            System.out.println(lastCol);
+            // For each row, iterate through each columns
+            Iterator<Cell> cellIterator = row.cellIterator();
+
+            while (cellIterator.hasNext()) {
+
+                Cell cell = cellIterator.next();
+                if(cell.getRowIndex()==0){
+                    continue;
+                }
+
+                switch (cell.getCellType()) {
+                    case STRING:
+                        System.out.print(cell.getStringCellValue() + "\t");
+                        if(cell.getColumnIndex()+1== lastCol){
+                            sb.append("01"+"|"+cell.getStringCellValue());
+                        }else {
+                            sb.append("01"+"|"+cell.getStringCellValue() + "|");
+                            System.out.println("masuk else");
+                        }
+                        break;
+                    case NUMERIC:
+                        System.out.print((int) cell.getNumericCellValue() + "\t");
+                        if(cell.getColumnIndex()+1== lastCol){
+                            sb.append("01"+"|"+(int)cell.getNumericCellValue());
+                        }else{
+                            sb.append("01"+"|"+(int)cell.getNumericCellValue()+ "|");
+                            System.out.println("masuk else");
+
+                        }
+                        break;
+//                    case BOOLEAN:
+//                        System.out.print(cell.getBooleanCellValue() + "\t");
+//
+//                        if(cell.getColumnIndex()+1== lastCol){
+//                            sb.append("01"+"|"+cell.getBooleanCellValue());
+//                        }else{
+//                            sb.append("01"+"|"+cell.getBooleanCellValue()+ "|");
+//                        }
+//                        break;
+                    default :
+
+                }
+            }
+            System.out.println("");
+            sb.append("\n");
+        }
+        sb.append(99+"|"+ 3+"|"+0+"\n");
+
+        Path path = Paths.get(filePath +"INCIFVIP_"+localDate+"_"+approvedData.getFileId()+".txt");
+        Files.write(path, Arrays.asList(sb.toString()));
+
+
+
+    }
+
+
 }
+
+
